0164c02c971b40b45b4b88de9b66c5ad57b1bd9a
Ugo Paternostro <ugo.git@paternostro.org>
2026-07-17 13:27:29 +0200

    ElkrommFacade: pass data to enqueuePayload

--- src/main/java/org/paternostro/elkromm/impl/ElkrommFacadeImpl.java
+++ src/main/java/org/paternostro/elkromm/impl/ElkrommFacadeImpl.java
@@ -204,7 +204,7 @@ public class ElkrommFacadeImpl implements ElkrommFacade
         if (status != Status.ST_LOGGED_IN) throw new AssertionError("Wrong status");
 
         try {
-            packetQueue.enqueuePayload(ElkrommPacket.packetFactoryAllocate(command, bcdPlantCode.get(0), bcdPlantCode.get(1), 0, 0, 0, null), data);
+            packetQueue.enqueuePayload(ElkrommPacket.packetFactoryAllocate(command, bcdPlantCode.get(0), bcdPlantCode.get(1), 0, 0, data == null ? 0 : data.length, data), data);
 
             while (!packetQueue.isEmpty()) {
                 Thread.sleep(DELAY);
