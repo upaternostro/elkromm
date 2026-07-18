ea19255cae750ce66c634ea54f0628ec2e3b4748
Ugo Paternostro <ugo.git@paternostro.org>
2026-07-18 19:30:46 +0200

    Fix SetAreasAndPartitions (data length cannot be > 140)

--- src/main/java/org/paternostro/elkromm/packet/SetAreasAndPartitions.java
+++ src/main/java/org/paternostro/elkromm/packet/SetAreasAndPartitions.java
@@ -8,6 +8,6 @@ public class SetAreasAndPartitions extends ElkrommPacket {
 
         assert totalPackets == 2 : "SetAreasAndPartitions: totalPackets: expected 2, found " + totalPackets;
         assert index >= 0 && index <= 2 : "SetAreasAndPartitions: index: expected 0, 1 or 2, found " + index;
-        assert dataLength == 333 && data != null && data.length == dataLength : "SetAreasAndPartitions: data: mismatch in data length";
+        assert dataLength > 0 && data != null && data.length == dataLength : "SetAreasAndPartitions: data: mismatch in data length";
     }
 }
